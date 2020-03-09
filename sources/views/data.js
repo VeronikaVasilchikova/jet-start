fbd1e95b1538cb1f0a2af99e458c5d63a84a7e24
VeronikaVasilchikova <veroni4ka.v@gmail.com>
2020-03-09 10:20:15 +0300

    Fix onChange

--- sources/views/data.js
+++ sources/views/data.js
@@ -7,16 +7,12 @@ export default class DataView extends JetView{
 		const tabbar = {
 			view: "tabbar",
 			value: "Countries",
+			localId: "tabbar",
 			options: [
-				{ value: "Countries", localId: "countries" },
-				{ value: "Statuses", localId: "statuses" },
+				{ value: "Countries" },
+				{ value: "Statuses" },
 			],
-			height: 50,
-			on: {
-				onChange(id) {
-					this.$$(id).show();
-				}
-			}
+			height: 50
 		};
 
 		return {
@@ -30,11 +26,14 @@ export default class DataView extends JetView{
 				tabbar,
 				{
 					cells: [
-						{localId: "countries", rows: [new GridView(this.app, "", countries)]},
-						{localId: "statuses", rows: [new GridView(this.app, "", statuses) ]}
+						{localId: "Countries", rows: [new GridView(this.app, "", countries)]},
+						{localId: "Statuses", rows: [new GridView(this.app, "", statuses) ]}
 					]
 				}
 			]
 		};
+	}
+	init() {
+		this.$$("tabbar").attachEvent("onChange", value => this.$$(value).show());
 	}
 }
