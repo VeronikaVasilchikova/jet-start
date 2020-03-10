b434c8209c7645cebd678a07b86cc35f775264ea
VeronikaVasilchikova <veroni4ka.v@gmail.com>
2020-03-10 11:53:44 +0300

    Create collections and sync Contact List with collection

---
+++ sources/models/countriesCollection.js
@@ -0,0 +1,4 @@
+export const countries = new webix.DataCollection({
+	url: "http://localhost:8096/api/v1/countries/",
+	save: "http://localhost:8096/api/v1/countries/"
+});
--- sources/views/contacts.js
+++ sources/views/contacts.js
@@ -1,5 +1,5 @@
 import { JetView } from "webix-jet";
-import { contacts } from "models/contacts";
+import { contacts } from "models/contactsCollection";
 
 export default class ContactsView extends JetView {
 	config() {
@@ -57,13 +57,15 @@ export default class ContactsView extends JetView {
 		});
 	}
 	urlChange(view, url){
-		let id = contacts.getFirstId();
+		contacts.waitData.then(()=>{
+			let id = contacts.getFirstId();
 
-		if(url[1] && url[1].params.id)
-			id = url[1].params.id;
+			if(url[1] && url[1].params.id)
+				id = url[1].params.id;
 
-		if(id && this.listOfUsers.exists(id)){
-			this.listOfUsers.select(id);
-		}
+			if(id && this.listOfUsers.exists(id)){
+				this.listOfUsers.select(id);
+			}
+		});
 	}
 }
